7160c4db22b6fa5c245701c0a9f8985b7cb8d68c
Ethan Hathaway <erhathaway@gmail.com>
2018-08-20 18:46:43 -0700

    add commonjs parsing and include query-string directly

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,26 @@
 import babel from 'rollup-plugin-babel';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
 
 const dependencies = Object.keys(require('./package.json').dependencies);
-
+const include = 'query-string';
+console.log(dependencies)
 export default {
   input: 'src/index.js',
   output: {
     file: 'dist/index.js',
     format: 'cjs'
   },
-  external: dependencies,
   plugins: [
+    resolve({
+      only: 'query-string'
+    }),
+    commonjs({
+      include: 'node_modules/**'
+    }),
     babel({
-      babelrc: false,
-      presets: [['env', { modules: false }]]
+      babelrc: true,
     })
-  ]
+  ],
+  external: dependencies.filter(d => d !== 'query-string'),
 };
