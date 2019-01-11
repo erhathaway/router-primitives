c7854db8dd7c647fc873e17a3d8cf9433427a2da
Ethan Hathaway <erhathaway@gmail.com>
2019-01-10 19:59:33 -0800

    move playground to seperate repo

--- examples/react-example/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Scenes from './Scenes';
-
-import './index.css';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<Scenes />, document.getElementById('root'));
-registerServiceWorker();
